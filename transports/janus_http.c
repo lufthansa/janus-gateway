fd922b27eb31d20efa6b1c0bf3cfc010815b9e37
lufthansa <lufthansa.163.com>
2019-11-07 13:52:09 +0800

    debug

--- transports/janus_http.c
+++ transports/janus_http.c
@@ -1149,6 +1149,7 @@ int janus_http_admin_client_connect(void *cls, const struct sockaddr *addr, sock
 /* WebServer requests handler */
 int janus_http_handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **ptr)
 {
+    JANUS_LOG(LOG_INFO, "in janus_http_handler\n");
 	char *payload = NULL;
 	json_t *root = NULL;
 	struct MHD_Response *response = NULL;
@@ -1522,6 +1523,7 @@ done:
 /* Admin/monitor WebServer requests handler */
 int janus_http_admin_handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **ptr)
 {
+    JANUS_LOG(LOG_INFO, "in janus_http_admin_handler\n");
 	char *payload = NULL;
 	json_t *root = NULL;
 	struct MHD_Response *response = NULL;
